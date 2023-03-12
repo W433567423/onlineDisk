ec162c593f38eb730f8ca460e39c4135f43bd1f5
tutu <t433567423@163.com>
2023-03-12 17:01:43 +0800

    feat: tarbar of list page

--- pages/list/list.vue
+++ pages/list/list.vue
@@ -1,10 +1,28 @@
 <template>
-	<view class="list">
-		<uniStatusBar></uniStatusBar>
-		list
+	<view style="height: 100vh" class="list flex flex-column">
+		<view class="flex border-bottom border-light-secondary" style="height: 100rpx;">
+			<view
+				class="flex-1 flex flex-column align-center justify-center font-md "
+				:class="tarbarIndex == index ? 'text-main' : ''"
+				v-for="(item, index) in tarbarOptions"
+				@click="handleChangeTarbar(index)"
+				:key="index"
+			>
+				<text>{{ item.name }}</text>
+				<span class=" rounded" style="width: 30rpx;height: 8rpx;" :class="tarbarIndex == index ? 'bg-main' : 'bg-white'"></span>
+			</view>
+		</view>
 	</view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+// 导航
+const tarbarIndex = ref(0)
+const tarbarOptions = ref([{ name: '下载列表' }, { name: '上传列表' }])
+const handleChangeTarbar = (index: number) => {
+	tarbarIndex.value = index
+}
+</script>
 
 <style lang="less" scoped></style>
